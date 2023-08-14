7a54f11e47ebedf74664eff635bafcb388fc9257
wade-profe <wade.profe@gmail.com>
2023-08-14 09:16:09 +0200

    Initial commit

---
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.udacity
+
+import android.app.DownloadManager
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import com.udacity.databinding.ActivityMainBinding
+import com.udacity.databinding.ContentMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var contentBinding: ContentMainBinding
+
+    private var downloadID: Long = 0
+
+    private lateinit var notificationManager: NotificationManager
+    private lateinit var pendingIntent: PendingIntent
+    private lateinit var action: NotificationCompat.Action
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        contentBinding = ContentMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        setSupportActionBar(mainBinding.toolbar)
+
+        registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+
+        contentBinding.customButton.setOnClickListener {
+            download()
+        }
+    }
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+        }
+    }
+
+    private fun download() {
+        val request =
+            DownloadManager.Request(Uri.parse(URL))
+                .setTitle(getString(R.string.app_name))
+                .setDescription(getString(R.string.app_description))
+                .setRequiresCharging(false)
+                .setAllowedOverMetered(true)
+                .setAllowedOverRoaming(true)
+
+        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        downloadID =
+            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+    }
+
+    companion object {
+        private const val URL =
+            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val CHANNEL_ID = "channelId"
+    }
+}
